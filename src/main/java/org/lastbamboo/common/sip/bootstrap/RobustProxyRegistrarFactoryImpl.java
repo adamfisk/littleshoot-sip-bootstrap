aeaa3675f367478ed4b905ce6a087a64fa7b57fc
adamfisk <a@littleshoot.org>
2010-05-18 06:05:19 +0000

    Tweaks to improve how we handle DNS SRV record lookups and changed to generalize SRV code
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/sip/bootstrap@7008 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/sip/bootstrap/RobustProxyRegistrarFactoryImpl.java
+++ src/main/java/org/lastbamboo/common/sip/bootstrap/RobustProxyRegistrarFactoryImpl.java
@@ -1,16 +1,33 @@
 package org.lastbamboo.common.sip.bootstrap;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.URI;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import org.lastbamboo.common.sip.client.util.ProxyRegistrationListener;
+import org.lastbamboo.common.sip.stack.util.UriUtils;
 import org.lastbamboo.common.util.CandidateProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * An implementation of the robust proxy registrar factory implementation.
  */
 public final class RobustProxyRegistrarFactoryImpl
-        implements RobustProxyRegistrarFactory
+    implements RobustProxyRegistrarFactory
     {
+    
+    private final Logger m_log = LoggerFactory.getLogger(getClass());
+    
+    /**
+     * The default transport to use to connect to this host.
+     */
+    private static final String DEFAULT_TRANSPORT = "tcp";
+
+    private final UriUtils m_uriUtils;
+    
     /**
      * The candidate provider that provides candidate registrars for
      * registration.
@@ -24,17 +41,53 @@ public final class RobustProxyRegistrarFactoryImpl
 
     /**
      * Constructs a new robust proxy registrar factory.
-     *
-     * @param candidateProvider The candidate provider that provides candidate 
+     * 
+     * @param uriUtils Utilities for creating SIP URIs.
+     * @param sipCandidateProvider The candidate provider that provides candidate 
      * registrars for registration.
      * @param registrarFactory The registrar factory that provides registrars 
      * for single registrations.
      */
-    public RobustProxyRegistrarFactoryImpl (
-        final CandidateProvider<URI> candidateProvider,
+    public RobustProxyRegistrarFactoryImpl (final UriUtils uriUtils,
+        final CandidateProvider<InetSocketAddress> sipCandidateProvider,
         final ProxyRegistrarFactory registrarFactory)
         {
-        this.m_candidateProvider = candidateProvider;
+        this.m_uriUtils = uriUtils;
+        this.m_candidateProvider = new CandidateProvider<URI>() 
+            {
+            public URI getCandidate() 
+                {
+                final Collection<URI> candidates = getCandidates();
+                if (candidates.isEmpty()) return null;
+                return candidates.iterator().next();
+                }
+
+            public Collection<URI> getCandidates() 
+                {
+                m_log.debug("Accessing SIP servers...");
+                final Collection<URI> candidates = new LinkedList<URI>();
+                
+                final Collection<InetSocketAddress> addresses =
+                    sipCandidateProvider.getCandidates();
+                
+                for (final InetSocketAddress isa : addresses)
+                    {
+                    final InetAddress address = isa.getAddress();
+                    
+                    // The URI we are given is the public address (and SIP port) of
+                    // this host. To convert to the URI of the proxy we are running,
+                    // we replace the port with the proxy port.
+                    final String host = address.getHostAddress();
+                
+                    final URI uri = m_uriUtils.getSipUri(host, isa.getPort(), 
+                        DEFAULT_TRANSPORT);
+                    candidates.add(uri);
+                    }
+                
+                return candidates;
+                }
+            };
+            
         this.m_registrarFactory = registrarFactory;
         }
 
