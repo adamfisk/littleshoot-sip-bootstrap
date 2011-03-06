12ebb0e6d3e7c2c74dd4ef0f511ea3d373db4378
Adam Fisk <a@littleshoot.org>
2011-03-05 19:45:12 -0800

    Formatting

--- src/main/java/org/lastbamboo/common/sip/bootstrap/SipClientLauncher.java
+++ src/main/java/org/lastbamboo/common/sip/bootstrap/SipClientLauncher.java
@@ -20,8 +20,7 @@ import org.lastbamboo.common.sip.stack.SipUriFactory;
 /**
  * This class kicks off all SIP client services.
  */
-public final class SipClientLauncher implements P2PClient
-    {
+public final class SipClientLauncher implements P2PClient {
 
     /**
      * The log for this class.
@@ -55,17 +54,17 @@ public final class SipClientLauncher implements P2PClient
      * @param offerAnswerFactory Factory for creating offers and answers.
      * @param relayWaitTime The time to wait before using a relay.
      */
-    public SipClientLauncher(final SipClientTracker sipClientTracker, 
-        final RobustProxyRegistrarFactory registrarFactory,
-        final SipUriFactory sipUriFactory, 
-        final OfferAnswerFactory offerAnswerFactory, final int relayWaitTime)
-        {
+    public SipClientLauncher(final SipClientTracker sipClientTracker,
+            final RobustProxyRegistrarFactory registrarFactory,
+            final SipUriFactory sipUriFactory,
+            final OfferAnswerFactory offerAnswerFactory, 
+            final int relayWaitTime) {
         this.m_sipClientTracker = sipClientTracker;
         this.m_registrarFactory = registrarFactory;
         this.m_sipUriFactory = sipUriFactory;
         this.m_offerAnswerFactory = offerAnswerFactory;
         this.m_relayWaitTime = relayWaitTime;
-        }
+    }
 
     /**
      * Registers a given user ID with SIP proxies so that other people can
@@ -73,13 +72,12 @@ public final class SipClientLauncher implements P2PClient
      *
      * @param userId The identifier of the user to register.
      */
-    public void register(final long userId)
-        {
+    public void register(final long userId) {
         LOG.debug("Registering...");
         // Set up the URI used as the 'From' for SIP messages.
-        final URI sipUri = m_sipUriFactory.createSipUri (userId);
+        final URI sipUri = m_sipUriFactory.createSipUri(userId);
         register(sipUri);
-        }
+    }
 
     /**
      * Registers a given user ID with SIP proxies so that other people can
@@ -87,15 +85,13 @@ public final class SipClientLauncher implements P2PClient
      *
      * @param userId The identifier of the user to register.
      */
-    public void register(final URI sipUri) 
-        {
+    public void register(final URI sipUri) {
         // Register with the SIP network.
-        final RobustProxyRegistrar registrar =
-            m_registrarFactory.getRegistrar
-                (sipUri, new NoOpRegistrationListener());
+        final RobustProxyRegistrar registrar = m_registrarFactory.getRegistrar(
+                sipUri, new NoOpRegistrationListener());
 
-        registrar.register ();
-        }
+        registrar.register();
+    }
 
     /**
      * Registers a given user ID with SIP proxies so that other people can
@@ -103,77 +99,64 @@ public final class SipClientLauncher implements P2PClient
      *
      * @param userId The identifier of the user to register.
      */
-    public void register(final String id)
-        {
+    public void register(final String id) {
         LOG.debug("Registering...");
         // Set up the URI used as the 'From' for SIP messages.
-        final URI sipUri = m_sipUriFactory.createSipUri (id);
+        final URI sipUri = m_sipUriFactory.createSipUri(id);
         register(sipUri);
-        }
-    
-    private static final class NoOpRegistrationListener 
-        implements ProxyRegistrationListener
-        {
+    }
 
-        public void reRegistered(URI client, URI proxy)
-            {
+    private static final class NoOpRegistrationListener implements
+            ProxyRegistrationListener {
+
+        public void reRegistered(URI client, URI proxy) {
             LOG.debug("Got re-registered");
-            }
+        }
 
-        public void registered(URI client, URI proxy)
-            {
+        public void registered(URI client, URI proxy) {
             LOG.debug("Got registered");
-            }
+        }
 
-        public void registrationFailed(URI client, URI proxy)
-            {
+        public void registrationFailed(URI client, URI proxy) {
             LOG.debug("Got registration failed.");
-            }
+        }
 
-        public void unregistered(URI client, URI proxy)
-            {
+        public void unregistered(URI client, URI proxy) {
             LOG.debug("Got unregistered");
-            }
-        
-        }
-
-    public void offer(final URI sipUri, final byte[] offer,
-        final OfferAnswerTransactionListener transactionListener) 
-        {
-        LOG.error("Offer not supported");
-        throw new UnsupportedOperationException("Offer not supported");
-        }
-
-    public String login(final String user, final String password) 
-        {
-        LOG.error("Login not supported");
-        throw new UnsupportedOperationException("Login not supported");
-        }
-    
-    public String login(final String user, final String password, 
-        final String id) throws IOException 
-        {
-        LOG.error("Login not supported");
-        throw new UnsupportedOperationException("Login not supported");
-        }
-
-    public Socket newSocket (final URI sipUri) throws IOException, 
-        NoAnswerException
-        {
-        LOG.trace ("Creating SIP socket for URI: {}", sipUri);
-        final SipClient client = this.m_sipClientTracker.getSipClient();
-        if (client == null)
-            {
-            LOG.warn("No available SIP clients!!");
-            throw new IOException (
-                "No available connections to SIP proxies!!");
-            }
-        
-        final TcpUdpSocket tcpUdpSocket = 
-            new DefaultTcpUdpSocket(client, this.m_offerAnswerFactory,
-                this.m_relayWaitTime);
-        
-        return tcpUdpSocket.newSocket(sipUri);
         }
 
     }
+
+    public void offer(final URI sipUri, final byte[] offer,
+            final OfferAnswerTransactionListener transactionListener) {
+        LOG.error("Offer not supported");
+        throw new UnsupportedOperationException("Offer not supported");
+    }
+
+    public String login(final String user, final String password) {
+        LOG.error("Login not supported");
+        throw new UnsupportedOperationException("Login not supported");
+    }
+
+    public String login(final String user, final String password,
+            final String id) throws IOException {
+        LOG.error("Login not supported");
+        throw new UnsupportedOperationException("Login not supported");
+    }
+
+    public Socket newSocket(final URI sipUri) throws IOException,
+            NoAnswerException {
+        LOG.trace("Creating SIP socket for URI: {}", sipUri);
+        final SipClient client = this.m_sipClientTracker.getSipClient();
+        if (client == null) {
+            LOG.warn("No available SIP clients!!");
+            throw new IOException("No available connections to SIP proxies!!");
+        }
+
+        final TcpUdpSocket tcpUdpSocket = new DefaultTcpUdpSocket(client,
+                this.m_offerAnswerFactory, this.m_relayWaitTime);
+
+        return tcpUdpSocket.newSocket(sipUri);
+    }
+
+}
